4bcf34ca832b0866d882fd9f0c1cf1b6c903c69b
Joey Stoffels <joey.stoffels@exe.energy>
2018-10-24 17:35:29 +0200

    compare sentences

--- src/plapp/core/Plapp.java
+++ src/plapp/core/Plapp.java
@@ -3,7 +3,6 @@ package plapp.core;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -21,24 +20,52 @@ public class Plapp {
 
     private Plapp() {
         String baseDir = "C:/Users/Joey/Google Drive/plapp/";
-        File file = new File(baseDir + "docA.docx");
+        File firstFile = new File(baseDir + "docA.docx");
+        File secondFile = new File(baseDir + "docB.docx");
 
         try {
-            printFileContent(file);
+            getFileContent(firstFile);
+            LOGGER.info("Number of identical sentences: " + compareFiles(firstFile, secondFile));
         } catch (FileNotFoundException e) {
             LOGGER.error("File not found!");
         }
     }
 
-    private void printFileContent(File file) throws FileNotFoundException {
+    private int compareFiles(File firstFile, File secondFile) throws FileNotFoundException {
+
+        int counter = 0;
+
+        String[] sentencesFirstFile = getSentences(firstFile);
+        String[] sentencesSecondFile = getSentences(secondFile);
+
+        for (String sentenceFirstFile : sentencesFirstFile) {
+            for (String sentenceSecondFile : sentencesSecondFile) {
+                if (sentenceFirstFile.equals(sentenceSecondFile)) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    private String getFileContent(File file) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(file);
+        String content = "";
 
         try (XWPFDocument doc = new XWPFDocument(fis)) {
             XWPFWordExtractor we = new XWPFWordExtractor(doc);
-            LOGGER.info(we.getText());
+            content = we.getText();
             we.close();
         } catch (Exception e) {
             LOGGER.error("Error while printing content!");
         }
+
+        return content;
     }
+
+    private String[] getSentences(File file) throws FileNotFoundException {
+        return getFileContent(file).split("\\s*[.!?]+\\s*");
+    }
+
 }
